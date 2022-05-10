347b5c87721b9f845d3d555ef4c86e56da551103
Jorge Puerta <jorge.puerta00@gmail.com>
2022-05-10 06:41:18 -0500

    first commit

---
+++ scripts/create-topic.sh
@@ -0,0 +1,13 @@
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --zookeeper zookeeper:2181 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic test
+
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic test
